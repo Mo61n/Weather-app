fc1563640926f73af960e5f593f8bb2c6b416bd0
mo6in <108945415+Mo61n@users.noreply.github.com>
2024-03-15 11:15:59 +0330

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,50 +1,53 @@
 document
-        .getElementById("location")
-        .addEventListener("keypress", function (event) {
-          if (event.keyCode === 13) {
-            event.preventDefault();
-            if (document.getElementById("location").value.trim() !== "") {
-              getWeatherInfo();
-            } else {
-              var alertDiv = document.getElementById("empty-input-alert");
-              alertDiv.style.display = "block";
-              setTimeout(function () {
-                alertDiv.style.display = "none";
-              }, 3500);
-            }
-          }
-        });
+.getElementById("location")
+.addEventListener("keypress", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    if (document.getElementById("location").value.trim() !== "") {
+      getWeatherInfo();
+    } else {
+      var alertDiv = document.getElementById("empty-input-alert");
+      alertDiv.style.display = "block";
+      setTimeout(function () {
+        alertDiv.style.display = "none";
+      }, 3500);
+    }
+  }
+});
 
-      function getWeatherInfo() {
-        var location = document.getElementById("location").value.trim();
-        // You can make your API key in https://weatherstack.com and paste into "API-KEY"
-        var access_key = "API-KEY";
-        var url =
-          "http://api.weatherstack.com/current?access_key=" +
-          access_key +
-          "&query=" +
-          location;
-        $.get(url, function (data) {
-          if (data.success !== false) {
-            var temp = data.current.temperature;
-            var desc = data.current.weather_descriptions[0];
-            var icon = data.current.weather_icons[0];
-            var info =
-              "<h3>Current Weather in " +
-              location +
-              "</h3><br><img src=" +
-              icon +
-              "><br>" +
-              temp +
-              " °C<br>" +
-              desc;
-            $("#weather-info").html(info);
-          } else {
-            var errorInfo =
-              "<h3>Oops, we couldn't find any information for " +
-              location +
-              ". Please try again.</h3>";
-            $("#weather-info").html(errorInfo);
-          }
-        });
-      }
+function getWeatherInfo() {
+  var location = document.getElementById("location").value.trim();
+  var access_key = "edd9a63772014bea9cb73011241503";
+  var url =
+    "https://api.weatherapi.com/v1/current.json?key=" +
+    access_key +
+    "&q=" +
+    location;
+  $.get(url, function (data) {
+    if (data) {
+      var temp = data.current.temp_c;
+      var desc = data.current.condition.text;
+      var icon = "https://" + data.current.condition.icon.substring(2);
+      var info =
+        "<h3>Current Weather in " +
+        location +
+        "</h3><br><img src=" +
+        icon +
+        "><br>" +
+        temp +
+        " °C<br>" +
+        desc;
+      $("#weather-info").html(info);
+    } else {
+      var errorInfo =
+        "<h3>Oops, we couldn't find any information for " +
+        location +
+        ". Please try again.</h3>";
+      $("#weather-info").html(errorInfo);
+    }
+  }).fail(function() {
+      var errorInfo =
+        "<h3>Oops, there was an error retrieving the weather information. Please try again.</h3>";
+      $("#weather-info").html(errorInfo);
+  });
+}
